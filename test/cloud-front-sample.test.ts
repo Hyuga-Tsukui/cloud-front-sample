4ffd31ff849c563210ee0edb15958ac0f13cfbb3
Hyuga Tsukui <gakikiki407@gmail.com>
2023-07-21 22:56:36 +0900

    Initial commit

---
+++ test/cloud-front-sample.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CloudFrontSample from '../lib/cloud-front-sample-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cloud-front-sample-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CloudFrontSample.CloudFrontSampleStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
